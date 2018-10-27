5836b9fea2ce6bd14bc59b99b51977d3ca3596de
tejus-gupta <tejusgupta14@gmail.com>
2018-10-27 12:34:59 +0530

    added setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='pytorch-spear',
     version='0.1.0',
     description='Library for benchmarking adversarial defence methods',
+    url='http://github.com/tejus-gupta/spear',
     author='Tejus Gupta',
     license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'torchvision',
+    ],
+    include_package_data=True,
+    zip_safe=False,
 )
